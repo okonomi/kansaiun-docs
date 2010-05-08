599cf36dd47d79ab95f5d6bff03fdc60e2898f0a
kawakami <kawakami@natsume-game.com>
2010-05-09 00:26:44 +0900

    PostgreSQLで使えないテーブル名だったので変更

--- ethna/sample250/schema/pizza_pgsql.sql
+++ ethna/sample250/schema/pizza_pgsql.sql
@@ -1,52 +1,52 @@
 -- Project Name : ピザやさん
--- Date/Time    : 2010/05/08 23:59:42
+-- Date/Time    : 2010/05/09 0:23:25
 -- Author       : shinya
 -- RDBMS Type   : PostgreSQL
 -- Application  : A5:SQL Mk-2
 
 -- 注文内容
-create table order_content (
+create table pizza_order_content (
   id serial not null
-  , order_id integer not null
+  , pizza_order_id integer not null
   , pizza_id integer not null
-  , constraint order_content_PKC primary key (id)
-) ;
+  , constraint pizza_order_content_PKC primary key (id)
+);
 
 -- ピザ
 create table pizza (
   id serial not null
   , constraint pizza_PKC primary key (id)
-) ;
+);
 
 -- 注文
-create table order (
+create table pizza_order (
   id serial not null
-  , user_id integer not null
+  , consumer_id integer not null
   , shop_id integer not null
-  , constraint order_PKC primary key (id)
-) ;
+  , constraint pizza_order_PKC primary key (id)
+);
 
 -- 店舗
 create table shop (
   id serial not null
   , constraint shop_PKC primary key (id)
-) ;
+);
 
 -- 客
-create table user (
+create table consumer (
   id serial not null
-  , constraint user_PKC primary key (id)
-) ;
+  , constraint consumer_PKC primary key (id)
+);
 
-alter table order_content
-  add constraint order_content_FK1 foreign key (pizza_id) references pizza(id);
+alter table pizza_order_content
+  add constraint pizza_order_content_FK1 foreign key (pizza_id) references pizza(id);
 
-alter table order_content
-  add constraint order_content_FK2 foreign key (order_id) references order(id);
+alter table pizza_order_content
+  add constraint pizza_order_content_FK2 foreign key (pizza_order_id) references pizza_order(id);
 
-alter table order
-  add constraint order_FK1 foreign key (shop_id) references shop(id);
+alter table pizza_order
+  add constraint pizza_order_FK1 foreign key (shop_id) references shop(id);
 
-alter table order
-  add constraint order_FK2 foreign key (user_id) references user(id);
+alter table pizza_order
+  add constraint pizza_order_FK2 foreign key (consumer_id) references consumer(id);
 

e1b3d00f409655a3cceb26859c41fcccfd652bc3
okonomi <oknm@okonomi.sakura.ne.jp>
2010-05-09 02:20:13 +0900

    注文のAppManager

---
+++ ethna/pizza/main/app/manager/Pizza_PizzaOrderManager.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Pizza_PizzaOrderManager.php
+ *
+ *  @author     okonomi
+ *  @package    Pizza
+ *  @version    $Id$
+ */
+
+/**
+ *  Pizza_PizzaOrderManager
+ *
+ *  @author     okonomi
+ *  @access     public
+ *  @package    Pizza
+ */
+class Pizza_PizzaOrderManager extends Ethna_AppManager
+{
+}
+?>
